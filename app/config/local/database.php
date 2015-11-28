13586be210c1fa95cc117b44a4f5b560c7dbd68d
Mango Uchida <makochang.u@gmail.com>
2015-11-28 16:34:42 +0900

    ...local/database.php for viewpj.

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,9 +23,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'database'  => 'cm',
+			'username'  => 'view',
+			'password'  => 'view1239',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
